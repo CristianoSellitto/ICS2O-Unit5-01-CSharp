2189c2fdb68a17ecd8e5ab1af7d84e936be0e976
CristianoSellit <99354780+CristianoSellitto@users.noreply.github.com>
2022-04-21 17:28:56 +0000

    Added program

--- main.cs
+++ main.cs
@@ -8,7 +8,23 @@ class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello World");
+        int choice;
+        int randomnumber;
+
+        System.Random random = new System.Random(); 
+        randomnumber = random.Next(1,6);
+        // Console.WriteLine("For debugging, the random number is " + randomnumber + ".");
+        Console.Write("Pick a number from 1 to 6: ");
+        choice = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("The number was " + randomnumber + "!");
+        if (choice == randomnumber)
+        {
+            Console.WriteLine("You got it right!");
+        }
+        else
+        {
+            Console.WriteLine("You got it wrong...");
+        }
         Console.WriteLine("\nFinished.");
     }
 }
